2e0b0f4d63378afeaa0420d18f8b07ae7da68be6
Saheed LASISI <colosian@yahoo.com>
2021-05-18 10:47:31 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+module "network" {
+  source  = "app.terraform.io/realsaid84-training/network/google"
+  version = "3.2.2"
+  # insert required variables here
+  network_name = "realsaid-network"
+  project_id = var.project
+  subnets = [
+  {
+    subnet_name   = "realsaid-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
